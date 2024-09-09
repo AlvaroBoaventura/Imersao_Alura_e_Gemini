9d941fbdc6fd0b9536c1b57cc96e47200b844ba4
AlvaroBoaventura <121638579+AlvaroBoaventura@users.noreply.github.com>
2024-09-09 20:35:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha, que conquistou o mundo com seu talento e carisma. Medalhista olímpica, ela é um dos maiores nomes do skate mundial.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8",
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou o Brasil com suas lutas emocionantes. Medalhista olímpica, ela é um exemplo de determinação e superação.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka",
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua habilidade e determinação. Bicampeã olímpica, ela é considerada a maior ginasta da história do Brasil.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro",
+    }  
+  ];
+
